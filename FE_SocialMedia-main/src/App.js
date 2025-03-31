bf377ae409d44acf3fe18fe74776ad420f9d2119
ngtam0602 <132118564+ngtam0602@users.noreply.github.com>
2025-03-31 14:16:35 +0700

    Add files via upload

--- FE_SocialMedia-main/src/App.js
+++ FE_SocialMedia-main/src/App.js
@@ -1,15 +1,16 @@
 import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { Home, Login, Profile, Register, ResetPassword } from "./pages";
+import { Home, Login, Profile, Register, ResetPassword, EditProfilePage } from "./pages";
 
 function Layout() {
   const { user } = useSelector((state) => state.user);
   const location = useLocation();
-  return user ?
-   <Outlet />
-    : 
-    <Navigate to='/login' state={{ from: location }} replace />;
 
+  return user?.token ? (
+    <Outlet />
+  ) : (
+    <Navigate to='/login' state={{ from: location }} replace />
+  );
 }
 
 function App() {
@@ -19,13 +20,14 @@ function App() {
     <div data-theme={theme} className='w-full min-h-[100vh]'>
       <Routes>
         <Route element={<Layout />}>
-          <Route path='/Home' element={<Home />} />
+          <Route path='/' element={<Home />} />
           <Route path='/profile/:id?' element={<Profile />} />
         </Route>
 
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path="/edit-profile" element={<EditProfilePage />} />
       </Routes>
     </div>
   );
