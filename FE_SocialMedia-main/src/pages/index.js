8b474fdf9404c6931df062f3ead8d4e4a4620c10
ngtam0602 <132118564+ngtam0602@users.noreply.github.com>
2025-03-31 14:15:59 +0700

    Add files via upload
    Tạo profile page riêng, thêm đường dẫn
    31/3

--- FE_SocialMedia-main/src/pages/index.js
+++ FE_SocialMedia-main/src/pages/index.js
@@ -3,5 +3,6 @@ import Home from "./Home";
 import Register from "./Register";
 import Profile from "./Profile";
 import ResetPassword from "./ResetPassword";
+import EditProfilePage from "./EditProfilePage";
 
-export { Login, Home, Register, Profile, ResetPassword };
+export { Login, Home, Register, Profile, ResetPassword, EditProfilePage };
