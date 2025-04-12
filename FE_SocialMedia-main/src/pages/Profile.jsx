01582c3e98606d5a97af9d1d79f66341180c8dde
ngtam0602 <132118564+ngtam0602@users.noreply.github.com>
2025-04-13 00:49:16 +0700

    Add files via upload

--- FE_SocialMedia-main/src/pages/Profile.jsx
+++ FE_SocialMedia-main/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { fetchUserById } from "../redux/userSlice";
 import {
   FriendsCard,
   Loading,
@@ -9,36 +10,60 @@ import {
   TopBar,
 } from "../components";
 import { posts } from "../assets/data";
+import { NoCover } from "../assets";
 
 const Profile = () => {
-  
   const { id } = useParams();
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
   // const { posts } = useSelector((state) => state.posts);
   const [userInfo, setUserInfo] = useState(user);
-  // console.log("Profile page loaded",userInfo.userId);
-  const [loading, setLoading] = useState(false);
+  const [ setLoading] = useState(false);
+
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchUserById(id));
+    }
+  }, [dispatch, id]);
+
+  console.log("current user: ", currentUser);
 
   const handleDelete = () => {};
   const handleLikePost = () => {};
-  // console.log("userInfo", userInfo.userId);
+
   return (
     <>
       <div className='home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-screen overflow-hidden'>
         <TopBar />
         <div className='w-full flex gap-2 lg:gap-4 md:pl-4 pt-5 pb-10 h-full'>
           {/* LEFT */}
-          <div className='hidden w-1/3 lg:w-1/4 md:flex flex-col gap-6 overflow-y-auto'>
-            <ProfileCard user={userInfo} />
+          {/*Thử dời vị trí của profìe care sang phân giữa để hợp lý hơn */}
+          <div className='hidden w-1/6 lg:w-1/7 md:flex flex-col gap-6 overflow-y-auto'>
+            {/*<ProfileCard user={userInfo} />
 
             <div className='block lg:hidden'>
-              <FriendsCard friends={userInfo?.userId} />
-            </div>
+              <FriendsCard friends={userInfo?.friends} />
+            </div>*/}
           </div>
 
           {/* CENTER */}
           <div className=' flex-1 h-full bg-orimary px-4 flex flex-col gap-6 overflow-y-auto'>
+            
+            {/*Load ảnh bìa */}
+            <img
+              src={currentUser?.profileCover?? NoCover}
+              alt={currentUser?.email}
+              className='w-[100%] h-[17.5rem] object-cover' 
+            />
+            {/*Vị trí mới của phần thông tin người dùng */}
+
+            <ProfileCard user={currentUser} />
+              <div className='block lg:hidden'>
+              <FriendsCard friends={currentUser?.friends} />
+              </div>
+
             {loading ? (
               <Loading />
             ) : posts?.length > 0 ? (
@@ -46,7 +71,7 @@ const Profile = () => {
                 <PostCard
                   post={post}
                   key={post?._id}
-                  user={user}
+                  user={currentUser}
                   deletePost={handleDelete}
                   likePost={handleLikePost}
                 />
@@ -60,7 +85,7 @@ const Profile = () => {
 
           {/* RIGHT */}
           <div className='hidden w-1/4 h-full lg:flex flex-col gap-8 overflow-y-auto'>
-            <FriendsCard userId={userInfo?.userId} />
+            <FriendsCard friends={userInfo?.friends} />
           </div>
         </div>
       </div>
