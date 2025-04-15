fcdd3b26a74b00ded6f44e616ed719be617c0e29
ngtam0602 <132118564+ngtam0602@users.noreply.github.com>
2025-04-15 07:51:40 +0700

    Update FileStorageService.java

--- BE_SocialMedia-main/social_media/src/main/java/com/example/social_media/service/FileStorageService.java
+++ BE_SocialMedia-main/social_media/src/main/java/com/example/social_media/service/FileStorageService.java
@@ -13,15 +13,30 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class FileStorageService {
     private final Path uploadDir;
+    private static final String profile_path = "uploads/profile_pics/";
+    private static final String cover_path = "uploads/cover_pics/";
+
+
+    public void createDirectories (){
+        try{
+            Files.createDirectories(Paths.get(profile_path));
+            Files.createDirectories(Paths.get(cover_path));
+        }
+        catch (IOException e){
+            throw new RuntimeException("Loi trong qua trinh tao thu muc", e);
+        }
+    }
 
     public FileStorageService() {
         // Sử dụng đường dẫn tuyệt đối và normalize
+
         this.uploadDir = Paths.get("uploads").toAbsolutePath().normalize();
         try {
             Files.createDirectories(uploadDir);
         } catch (Exception e) {
             throw new RuntimeException("Không thể tạo thư mục uploads: " + e.getMessage());
         }
+        createDirectories();
     }
 
     public String saveFile(MultipartFile file) throws IOException {
@@ -54,5 +69,39 @@ public class FileStorageService {
         Files.copy(file.getInputStream(), filePath);
 
         return "/uploads/" + fileName;
+    }
+    public String saveProfilepic(MultipartFile file) throws IOException{
+        if (file.isEmpty() || !isValidImage(file)) {
+            throw new IOException("Invalid file");
+        }
+
+        String fileExtension = getFileExtension(file.getOriginalFilename());
+        String fileName = UUID.randomUUID().toString() + "." + fileExtension;
+        Path filePath = Paths.get(profile_path, fileName);
+
+        Files.write(filePath, file.getBytes());
+
+        return fileName;
+    }
+    public String saveCoverpic(MultipartFile file) throws IOException{
+        if (file.isEmpty() || isValidImage(file)) {
+            throw new IOException("File khong hop le");
+        }
+
+        String fileExtension = getFileExtension(file.getOriginalFilename());
+        String filename = UUID.randomUUID().toString() + "." + fileExtension;
+        Path filePath = Paths.get(cover_path, filename);
+
+        Files.write(filePath, file.getBytes());
+
+        return filename;
+
+    }
+    private String getFileExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
+    private boolean isValidImage(MultipartFile file) throws IOException {
+        String mimeType = Files.probeContentType(Paths.get(file.getOriginalFilename()));
+        return mimeType != null && mimeType.startsWith("image/");
     }
 }
